c011672918c532c4abfc9adc3ee72d91d1e69828
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2024-03-11 16:50:23 +0300

    added localization and device info

---
+++ src/lib/functions.ts
@@ -0,0 +1,11 @@
+export function choose<U, T extends Record<string, U>>(
+  value: keyof T,
+  choices: T
+): U {
+  return choices[value];
+}
+
+export function dedupArray<T>(arr: T[]): T[] {
+  const s = new Set(arr);
+  return [...s];
+}
---
+++ src/platform/detection.ts
@@ -0,0 +1,20 @@
+import { Platform } from "react-native";
+import { getLocales } from "expo-localization";
+import { dedupArray } from "lib/functions";
+
+export const isIOS = Platform.OS === "ios";
+export const isAndroid = Platform.OS === "android";
+export const isNative = isIOS || isAndroid;
+export const devicePlatform = isIOS ? "ios" : isAndroid ? "android" : "web";
+export const isWeb = !isNative;
+export const isMobileWebMediaQuery = "only screen and (max-width: 1300px)";
+export const isMobileWeb =
+  isWeb &&
+  // @ts-ignore we know window exists -prf
+  global.window.matchMedia(isMobileWebMediaQuery)?.matches;
+
+export const deviceLocales = dedupArray(
+  getLocales?.()
+    .map?.((locale) => locale.languageCode)
+    .filter((code) => typeof code === "string")
+) as string[];
